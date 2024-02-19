3821c95ea6c149067b77eec5f4e04c3b90aff44f
Taylor Fausak <taylor@fausak.me>
2024-02-19 02:49:42 +0000

    Dynamically fetch the latest version

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
 const fs = require('fs/promises');
+const httpClient = require('@actions/http-client');
 const os = require('os');
 const process = require('process');
-const tc = require('@actions/tool-cache');
+const toolCache = require('@actions/tool-cache');
 
+const HTTP_CLIENT = new httpClient.HttpClient('@tfausak/cabal-gild-setup-action');
 const TOOL_NAME = 'cabal-gild';
-const LATEST_VERSION = '1.0.0.1';
 
 (async () => {
   try {
@@ -15,14 +16,15 @@ const LATEST_VERSION = '1.0.0.1';
 
     let version = core.getInput('version');
     if (!version || version === 'latest') {
-      version = LATEST_VERSION;
+      const response = await HTTP_CLIENT.getJson('https://api.github.com/repos/tfausak/cabal-gild/releases/latest');
+      version = response.result.tag_name;
     }
 
-    let dir = tc.find(TOOL_NAME, version);
+    let dir = toolCache.find(TOOL_NAME, version);
     if (!dir) {
-      const file = await tc.downloadTool(`https://github.com/tfausak/${TOOL_NAME}/releases/download/${version}/${TOOL_NAME}-${version}-${platform}-${architecture}${extension}`);
+      const file = await toolCache.downloadTool(`https://github.com/tfausak/cabal-gild/releases/download/${version}/cabal-gild-${version}-${platform}-${architecture}${extension}`);
       await fs.chmod(file, 0o755);
-      dir = await tc.cacheFile(file, `${TOOL_NAME}${extension}`, TOOL_NAME, version);
+      dir = await toolCache.cacheFile(file, `${TOOL_NAME}${extension}`, TOOL_NAME, version);
     }
 
     core.addPath(dir);
