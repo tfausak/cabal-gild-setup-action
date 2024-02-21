e35db4dbedbf50639b568db0baaea0e96096164c
Taylor Fausak <taylor@fausak.me>
2024-02-20 20:12:46 -0600

    Add some logging

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ const TOOL_NAME = 'cabal-gild';
 (async () => {
   try {
     const platform = process.platform;
+    core.info(JSON.stringify({ platform }));
     const architecture = os.arch();
+    core.info(JSON.stringify({ architecture }));
     const extension = platform === 'win32' ? '.exe' : '';
+    core.info(JSON.stringify({ extension }));
 
     let version = core.getInput('version');
     if (!version || version === 'latest') {
       const response = await HTTP_CLIENT.getJson('https://api.github.com/repos/tfausak/cabal-gild/releases/latest');
       version = response.result.tag_name;
     }
+    core.info(JSON.stringify({ version }));
 
     let dir = toolCache.find(TOOL_NAME, version);
     if (!dir) {
@@ -26,6 +30,7 @@ const TOOL_NAME = 'cabal-gild';
       await fs.chmod(file, 0o755);
       dir = await toolCache.cacheFile(file, `${TOOL_NAME}${extension}`, TOOL_NAME, version);
     }
+    core.info(JSON.stringify({ dir }));
 
     core.addPath(dir);
   } catch (error) {
