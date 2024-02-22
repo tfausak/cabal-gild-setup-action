da699033dc7a9201a18c86a4582496f44d230399
Taylor Fausak <taylor@fausak.me>
2024-02-22 10:28:59 -0600

    Support new compressed assets

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ const TOOL_NAME = 'cabal-gild';
     if (!version || version === 'latest') {
       const response = await HTTP_CLIENT.getJson('https://api.github.com/repos/tfausak/cabal-gild/releases/latest');
       version = response.result.tag_name;
+      version = '1.0.2.1'; // TODO
     }
     core.info(JSON.stringify({ version }));
 
     let dir = toolCache.find(TOOL_NAME, version);
     if (!dir) {
-      const file = await toolCache.downloadTool(`https://github.com/tfausak/cabal-gild/releases/download/${version}/cabal-gild-${version}-${platform}-${architecture}${extension}`);
-      await fs.chmod(file, 0o755);
-      dir = await toolCache.cacheFile(file, `${TOOL_NAME}${extension}`, TOOL_NAME, version);
+      const file = await toolCache.downloadTool(`https://github.com/tfausak/cabal-gild/releases/download/${version}/cabal-gild-${version}-${platform}-${architecture}.tar.gz`);
+      core.info(JSON.stringify({ file }));
+      const directory = await toolCache.extractTar(file);
+      core.info(JSON.stringify({ directory }));
+      dir = await toolCache.cacheFile(path.join(directory, `${TOOL_NAME}${extension}`), `${TOOL_NAME}${extension}`, TOOL_NAME, version);
     }
     core.info(JSON.stringify({ dir }));
 
