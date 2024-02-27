23046f727337e4e099bd302f08ae25b8649cd27e
Taylor Fausak <taylor@fausak.me>
2024-02-27 06:48:44 -0600

    Authorize GitHub API call (#2)
    * Remove unused import
    
    * Fix user agent
    
    * Add logging
    
    * Authorize GitHub API call
    
    * Fix variable interpolation
    
    * Use core platform helpers
    
    * Get token from environment
    
    * Temporarily add more logging
    
    * Revert "Use core platform helpers"
    
    This reverts commit 5f5f775658f36569c0cf35fe0e8e69a3a526fbdb.
    
    * Pass token as an input
    
    * Improve logging
    
    * Fix typo

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 const core = require('@actions/core');
-const fs = require('fs/promises');
 const httpClient = require('@actions/http-client');
 const os = require('os');
 const path = require('path');
 const process = require('process');
 const toolCache = require('@actions/tool-cache');
 
-const HTTP_CLIENT = new httpClient.HttpClient('@tfausak/cabal-gild-setup-action');
-const TOOL_NAME = 'cabal-gild';
+const USER = 'tfausak';
+const REPO = 'cabal-gild';
+
+const HTTP = new httpClient.HttpClient(`${USER}/${REPO}`);
+const TOOL = REPO;
 
 (async () => {
   try {
     const platform = process.platform;
     const architecture = os.arch();
     const extension = platform === 'win32' ? '.exe' : '';
+    core.info(`Platform is ${JSON.stringify(platform)}.`);
+    core.info(`Architecture is ${JSON.stringify(architecture)}.`);
 
     let version = core.getInput('version');
+    core.info(`Requested version is ${JSON.stringify(version)}.`);
     if (!version || version === 'latest') {
-      const response = await HTTP_CLIENT.getJson('https://api.github.com/repos/tfausak/cabal-gild/releases/latest');
+      core.info('Getting latest version ...');
+      const headers = {
+        Accept: 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
+      };
+      const token = core.getInput('token');
+      if (token) {
+        headers['Authorization'] = `Bearer ${token}`;
+      }
+      const response = await HTTP.getJson(
+        `https://api.github.com/repos/${USER}/${REPO}/releases/latest`,
+        headers);
       version = response.result.tag_name;
     }
+    core.info(`Actual version is ${JSON.stringify(version)}.`);
 
-    let dir = toolCache.find(TOOL_NAME, version);
-    if (!dir) {
-      const file = await toolCache.downloadTool(`https://github.com/tfausak/cabal-gild/releases/download/${version}/cabal-gild-${version}-${platform}-${architecture}.tar.gz`);
+    let dir = toolCache.find(TOOL, version);
+    if (dir) {
+      core.info('Using cached executable ...');
+    } else {
+      core.info('Downloading executable ...');
+      const file = await toolCache.downloadTool(
+        `https://github.com/${USER}/${REPO}/releases/download/${version}/${TOOL}-${version}-${platform}-${architecture}.tar.gz`);
       const directory = await toolCache.extractTar(file);
-      dir = await toolCache.cacheFile(path.join(directory, `${TOOL_NAME}${extension}`), `${TOOL_NAME}${extension}`, TOOL_NAME, version);
+      dir = await toolCache.cacheFile(
+        path.join(directory, `${TOOL}${extension}`),
+        `${TOOL}${extension}`,
+        TOOL,
+        version);
     }
+    core.info(`Executable installed in ${JSON.stringify(dir)}.`);
 
     core.addPath(dir);
   } catch (error) {
